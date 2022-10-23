8b955a4522719ae49f62f9c3820953a158d29f1a
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-23 13:42:16 +0530

    working without adding navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
