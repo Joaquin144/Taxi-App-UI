8ca557e632d7aeec2625604467cc63ee32f36e92
Joaquin144 <vvpp898.comco@gmail.com>
2022-10-27 02:28:32 +0530

    ui added

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,23 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import React from 'react';
-import tw from 'tailwind-react-native-classnames';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { SECRET_KEY } from '../secret';
-import { useDispatch } from 'react-redux';
-import { setDestination } from '../slices/navSlice';
-import { useNavigation } from '@react-navigation/native';
-import NavFavourites from './NavFavourites';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { SECRET_KEY } from "../secret";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+import NavFavourites from "./NavFavourites";
+import { Icon } from "react-native-elements";
 
 const NavigateCard = () => {
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
@@ -37,8 +44,8 @@ const NavigateCard = () => {
             returnKeyType={"search"}
             minLength={2}
             onPress={(data, details = null) => {
-              console.log(data);
-              console.log(details);
+              //console.log(data);
+              //console.log(details);
               dispatch(
                 setDestination({
                   location: details.geometry.location,
@@ -52,30 +59,49 @@ const NavigateCard = () => {
             onFail={(error) => console.error(error)}
           />
         </View>
-        <NavFavourites/>
+        <NavFavourites />
       </View>
-      <View>
-        
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+      >
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+          onPress={() => navigation.navigate('RideOptionsCard')}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
-}
+};
 
-export default NavigateCard
+export default NavigateCard;
 
 const toInputBoxStyles = StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        padding: 20,
-        fex: 0,
-    },
-    textInput: {
-        backgroundColor: "#DDDDDF",
-        borderRadius: 0,
-        fontSize: 18,
-    },
-    textInputContainer: {
-        paddingHorizontal: 20,
-        paddingBottom: 0,
-    },
+  container: {
+    backgroundColor: "white",
+    padding: 20,
+    fex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
 });
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,12 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import tw from "tailwind-react-native-classnames";
+import { TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "Taxi-X-123",
+    title: "Mini",
+    multiplier: 1,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.sEwMHjO3RG9O4ulc00P4GAHaHa%26pid%3DApi&f=1&ipt=cc119484227012b666c7f91f0a07f3cf68cc6fea0b734beb3513428dc26539b0&ipo=images",
+  },
+  {
+    id: "Taxi-XL-456",
+    title: "Standard",
+    multiplier: 1.2,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clker.com%2Fcliparts%2Fv%2Fd%2Fw%2FD%2FI%2FR%2Fgreen-sports-car-hi.png&f=1&nofb=1&ipt=6eab1da78774d9c95cf1b7f39a49b7ca8cc544285e2b3418ab549fb8eb19b625&ipo=images",
+  },
+  {
+    id: "Taxi-LUX-789",
+    title: "Large",
+    multiplier: 1.75,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2F4c9%2FEAM%2F4c9EAMMcE.jpeg&f=1&nofb=1&ipt=b7d1e8b6e5e62f6c2982bcebfffc5939f6fab73e1b4b2a89d86b3ac01da9d023&ipo=images",
+  },
+];
 
 const RideOptionsCard = () => {
-  return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
 
-export default RideOptionsCard
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 z-50 left-5 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row items-center justify-between px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: item.image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{item.title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>INR 99</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && 'bg-gray-300'}`}>
+            <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
